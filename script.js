aad39255dd797f4f46ceceb0d70a4c46b6470bd2
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-08-27 19:06:49 -0300

    feat(button): add event to buttons

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const memeText = document.getElementById('meme-text');
 const inputText = document.getElementById('text-input');
 const insertImage = document.getElementById('meme-insert');
 const memeImage = document.getElementById('meme-image');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+const memeContainer = document.getElementById('meme-image-container');
 
 inputText.addEventListener('input', () => {
   memeText.innerText = inputText.value;
@@ -9,4 +13,16 @@ inputText.addEventListener('input', () => {
 
 insertImage.addEventListener('input', () => {
   memeImage.src = URL.createObjectURL(insertImage.files[0]);
+});
+
+fireButton.addEventListener('click', () => {
+  memeContainer.style.border = '3px dashed red';
+});
+
+waterButton.addEventListener('click', () => {
+  memeContainer.style.border = '5px double blue';
+});
+
+earthButton.addEventListener('click', () => {
+  memeContainer.style.border = '6px groove green';
 });
