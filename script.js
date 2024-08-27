6e6466034bcd5725f5c5a85e6f63a604b508ae51
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-08-26 22:30:54 -0300

    feat(css): some css for view

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const memeText = document.getElementById('meme-text');
 const inputText = document.getElementById('text-input');
+const insertImage = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
 
 inputText.addEventListener('input', () => {
   memeText.innerText = inputText.value;
+});
+
+insertImage.addEventListener('input', () => {
+  memeImage.src = URL.createObjectURL(insertImage.files[0]);
 });
